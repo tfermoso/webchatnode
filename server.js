a3f71f704f10e80ed1e9f63679f3434c943d45e7
tfermoso <tomas.fermoso@gmail.com>
2024-05-23 19:40:33 +0200

    configuramos body-parser

--- server.js
+++ server.js
@@ -8,14 +8,17 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+// Configura el middleware para manejar datos JSON y datos codificados en URL
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 })
 
-app.post("/login",(req,res)=>{
+app.post("/login", (req, res) => {
     console.log(req);
     res.send("procesando login");
 })
