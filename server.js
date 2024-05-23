9e6477e638782084475408df0988fa75d4a19674
tfermoso <tomas.fermoso@gmail.com>
2024-05-23 18:57:15 +0200

    proyecto node

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+app.get("/login",(req,res)=>{
+    res.send("hola mundo");
+})
+
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
